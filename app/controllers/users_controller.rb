25e9161d6774479e26d36d81e6ce28d843a1de34
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2019-11-18 00:34:08 +0900

    DBにユーザーのデータを登録

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to action: :index
+    else
+      redirect_to action: :new
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :confirmationPassword)
   end
 end
