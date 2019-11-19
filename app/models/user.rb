8cc922b88567b4c76f21adb8173c1f4e7bde53b2
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2019-11-19 23:30:38 +0900

    入力フォームのvalidation実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { maximum: 15 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true, presence: true, uniqueness: true, length: { minimum: 6, maximum: 8 }
 end
